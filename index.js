e00b120ba18690d53821f27eb5d41f92906219e6
milocrosby <147284832+milocrosby@users.noreply.github.com>
2024-12-22 13:05:39 -0700

    Add files via upload
    0.0.0.2 release

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-// Initialize the counter variable
-let yamCounter = 0;
-let perClick = 1;
+
 // Select the button and counter display elements
 const yamButton = document.getElementById('yamButton');
 const yamCounterDisplay = document.getElementById('yamCounterDisplay');
@@ -17,14 +15,74 @@ yamButton.addEventListener('click', () => {
     yamCounterDisplay.textContent = yamCounter;
 });
 
+// Ymas Passive growth
+setInterval(() => {
+yamCounter = yamCounter + yamsPS
+yamCounterDisplay.textContent = yamCounter;
+}, 1000);
+
+
+
+// Yams to money
+sellAllYams.addEventListener('click', () => {
+
+    money = money + yamCounter
+    yamCounter = 0
+    moneyDisplay.textContent = money;
+    yamCounterDisplay.textContent = yamCounter;
+
+} )
+
+// Yams to money
+sellTenYams.addEventListener('click', () => {
+
+    tenYams = (yamCounter * .1)
+    tenYams = Math.floor (tenYams)
+
+    money = money + tenYams
+    yamCounter = yamCounter - tenYams
+    moneyDisplay.textContent = money;
+    yamCounterDisplay.textContent = yamCounter;
+
+} )
+
+
 clickPowerButton.addEventListener('click', () => {
     
-    // Increment the counter
-    if (yamCounter > 9) {
+    if (money >= clickPowerCost) {
     perClick = perClick + 1
-    yamCounter = yamCounter - 9
-    yamCounterDisplay.textContent = yamCounter;
+    money = money - clickPowerCost
+    moneyDisplay.textContent = money;
+    clickPowerCost = clickPowerCost * 1.5
+    clickPowerCost = Math.floor (clickPowerCost)
+    
+    clickPowerCostDisplay.textContent = clickPowerCost; 
 }
-    // Update the displayed counter value
     clickPowerDisplay.textContent = perClick;
+});
+
+wivesButton.addEventListener('click', () => {
+    
+    if (money >= wivesCostMoney && yamCounter >= wivesCostYams)  {
+    yamsPS = yamsPS + 1
+    yamsPSDisplay.textContent =   yamsPS
+
+    money = money - wivesCostMoney
+    yamCounter = yamCounter - wivesCostYams
+    moneyDisplay.textContent = money;
+    yamCounterDisplay.textContent = yamCounter;
+
+    wivesCostMoney = wivesCostMoney * 1.1
+    wivesCostMoney = Math.floor (wivesCostMoney)
+    wivesCostYams = wivesCostYams * 1.1
+    wivesCostYams = Math.floor (wivesCostYams)
+
+    wivesCostMoneyDisplay.textContent = wivesCostMoney
+    wivesCostYamsDisplay.textContent = wivesCostYams
+
+    
+   
+    
+
+}
 });
---
+++ init.js
@@ -0,0 +1,9 @@
+// Initialize the counter variable
+let yamCounter = 0;
+let yamsPS = 0;
+let perClick = 1;
+let clickPowerCost = 10 
+let money = 0
+let tenYams= 0
+let wivesCostMoney = 100
+let wivesCostYams = 10
